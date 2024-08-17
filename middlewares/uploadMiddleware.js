09e5c6d161804c514eaefabb036ca22cb0bd8d4e
Aditya Yadav <aditya023.ay@gmail.com>
2024-08-18 00:25:27 +0530

    upload file middleware

---
+++ middlewares/uploadMiddleware.js
@@ -0,0 +1,35 @@
+const multer = require("multer");
+const path = require("path");
+
+// Set up storage engine
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${path.extname(file.originalname)}`);
+  },
+});
+
+// File filter to validate file types
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|gif/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 1000000 }, // 1MB file size limit
+});
+
+module.exports = upload;
